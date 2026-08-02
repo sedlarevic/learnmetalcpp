5d8582161a4e9055d53807b70150cd2c60331e7f
Stefan Sedlarevic <raldesedlar@gmail.com>
2026-08-02 21:01:52 +0300

    init build

---
+++ LearnMetalAAPL/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  LearnMetalAAPL
+//
+//  Created by Stefan Sedlarevic on 2. 8. 2026..
+//
+
+#include <iostream>
+
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <AppKit/AppKit.hpp>
+#include <MetalKit/MetalKit.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
